2beaef73df983b9ece3eeea51a8f2e89bc381f31
bitflower <matthias.max@bitflower.net>
2020-10-16 18:51:22 +0200

    .

--- src/components/app-profile/app-profile.tsx
+++ src/components/app-profile/app-profile.tsx
@@ -3,7 +3,7 @@ import { sayHello } from '../../helpers/utils';
 
 @Component({
   tag: 'app-profile',
-  styleUrl: 'app-profile.css',
+  styleUrl: 'app-profile.css'
 })
 export class AppProfile {
   @State() state = false;
@@ -36,7 +36,25 @@ export class AppProfile {
           <ion-label>Setting ({this.state.toString()})</ion-label>
           <ion-toggle checked={this.state} onIonChange={ev => (this.state = ev.detail.checked)} />
         </ion-item>
-      </ion-content>,
+        <sl-form class="input-validation-required">
+          <sl-input name="name" label="Name" required></sl-input>
+
+          <sl-select label="Favorite Animal" clearable required>
+            <sl-menu-item value="birds">Birds</sl-menu-item>
+            <sl-menu-item value="cats">Cats</sl-menu-item>
+            <sl-menu-item value="dogs">Dogs</sl-menu-item>
+            <sl-menu-item value="other">Other</sl-menu-item>
+          </sl-select>
+
+          <sl-textarea name="comment" label="Comment" required></sl-textarea>
+
+          <sl-checkbox required>Check me before submitting</sl-checkbox>
+
+          <sl-button type="primary" submit>
+            Submit
+          </sl-button>
+        </sl-form>
+      </ion-content>
     ];
   }
 }
